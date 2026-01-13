517f7709519c974b46e3c606f898d81058e1e90f
yash srivastava <yashsrivastava087@gmail.com>
2026-01-14 03:47:36 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,35 @@
 import express, { urlencoded } from "express";
-import nodemon from "nodemon";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv";  
+import dotenv from "dotenv";
 import connectdb from "./utils/db.js";
+
 import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js"
-import jobRoute from "./routes/job.route.js"
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
-
-dotenv.config({});
+dotenv.config();
 
 const app = express();
 
-
 app.use(express.json());
 app.use(cookieParser());
-app.use(urlencoded({extended:true}))
-const corsoption = {
-    origin:'http://localhost:5173',
-    credentials: true
-}
+app.use(urlencoded({ extended: true }));
 
-app.use(cors(corsoption));
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/v1/user",userRoute);
+app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/job",jobRoute)
-app.use("/api/v1/application",applicationRoute)
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     connectdb();
-    console.log(`server running on port ${PORT}`);
-})
+    console.log(`Server running on port ${PORT}`);
+});
